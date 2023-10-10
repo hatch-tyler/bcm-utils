a05c0e19af61ca8e329ecf05fac393965cdfbc59
Tyler Hatch <hatch.tyler@gmail.com>
2023-10-10 10:59:48 -0700

    Add function clip_raster

--- src/bcm_utils/utilities.py
+++ src/bcm_utils/utilities.py
@@ -1,4 +1,3 @@
-import pathlib
 import fiona
 import rasterio
 import rasterio.mask
@@ -7,6 +6,7 @@ import numpy as np
 import pandas as pd
 import geopandas as gpd
 
+from pathlib import Path
 from shapely.geometry import Polygon
 
 
@@ -44,3 +44,85 @@ def create_bounding_polygon(area_of_interest, crs=None):
     bp = gpd.GeoDataFrame(geometry=[bounding_polygon], crs=aoi.crs)
 
     return bp
+
+
+def clip_raster(
+    raster: str,
+    raster_crs: str,
+    clip_boundary: str,
+    out_path: str,
+    out_ext: str | None = None,
+) -> None:
+    """
+    Clip a raster based on a provided clip boundary
+
+    Parameters
+    ----------
+    raster : str
+        file path and name of raster to clip
+
+    raster_crs: str
+        coordinate reference system for input raster
+
+    clip_boundary: str
+        geopandas-compatible format boundary to clip raster
+
+    out_path : str
+        file path to write clipped raster
+
+    out_ext : str | None, default None
+        raster file extension.
+
+        .. note valid formats can be determined via rasterio.drivers.raster_driver_extensions()
+
+    Returns
+    -------
+    None
+        writes the clipped raster to the output location
+    """
+
+    # get a list of geometries to use for clipping
+    shapes = gpd.read_file(clip_boundary).geometry.tolist()
+
+    with rasterio.open(raster, crs=raster_crs) as src:
+        out_image, out_transform = rasterio.mask.mask(src, shapes, crop=True)
+        out_meta = src.meta
+
+    if not out_ext:
+        # if an output file extension is not provided use the input raster format to specify the driver
+        driver = rasterio.drivers.driver_from_extension(raster)
+        raster_name = Path(raster).name
+
+    else:
+        # get the raster format driver from rasterio based on the provided raster file extension
+        raster_drivers = rasterio.drivers.raster_driver_extensions()
+        driver = raster_drivers[out_ext]
+        raster_name = f"{Path(raster).stem}.{out_ext}"
+
+    out_meta.update(
+        {
+            "driver": driver,
+            "height": out_image.shape[1],
+            "width": out_image.shape[2],
+            "transform": out_transform,
+            "crs": raster_crs,
+        }
+    )
+
+    # output raster path and name
+    clipped_raster = Path(out_path).joinpath(raster_name)
+
+    # write clipped raster to destination if it does not exist
+    if not clipped_raster.exists():
+        with rasterio.open(clipped_raster, "w", **out_meta) as dest:
+            dest.write(out_image)
+
+
+if __name__ == "__main__":
+    raster = r"D:\INTERA\BCM\rch\rch1899dec.asc"
+    bcm_crs = "EPSG:3310"
+    bp = r"D:\INTERA\Projects\Borrego\GIS\bounding_polygon.shp"
+    out_ext = "tif"
+    out_path = r"D:\INTERA\Projects\Borrego\BCM\test"
+
+    clip_raster(raster, bcm_crs, bp, out_path)
